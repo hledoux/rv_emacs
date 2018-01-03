f7ec22cef699ab92abd414f3fe6759a365115bd1
hledoux <herve.ledoux@gmail.com>
2018-01-03 17:43:41 +0000

    -

--- rv_emacs_faces.el
+++ rv_emacs_faces.el
@@ -7,6 +7,6 @@
 
 (defface font-lock-log-error-face '((t (:foreground "red"))) "shell-mode: error" :group 'font-lock-faces)
 (defface font-lock-log-in-out-face '((t (:weight bold :foreground "green"))) "shell-mode: ==> / <==" :group 'font-lock-faces)
-(defface font-lock-log-separ-face '((t (:foreground "Blue" :background "Gray90"))) "shell-mode: separator" :group 'font-lock-faces)
+(defface font-lock-log-separ-face '((t (:weight bold :foreground "green"))) "shell-mode: separator" :group 'font-lock-faces)
 (defface font-lock-shell-mode-context-face '((t (:weight bold :foreground "green" :background "gray20"))) "shell-mode: new prompt" :group 'font-lock-faces)
 
